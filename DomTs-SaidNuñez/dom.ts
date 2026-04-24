8f2f966a738e68b63250760bec9cd1179160d79c
Saidale <said14alejandro14@gmail.com>
2026-04-23 19:08:00 -0600

    Tarea:  dhtmllib: Funciones  para crear  20 elementos HTML diferentes

--- DomTs-SaidNuñez/dom.ts
+++ DomTs-SaidNuñez/dom.ts
@@ -50,6 +50,49 @@ document.body.appendChild(buttonId)
 document.body.appendChild(list);
 
 
+//----------------Tarea:  dhtmllib: Funciones  para crear  20 elementos HTML diferentes-------------------------------------
+function createDiv(id:string, clase:string):HTMLDivElement {
+    const div: HTMLDivElement = document.createElement("div");
+    div.id = id;
+    div.classList.add(clase);
+    div.textContent = "Un div con id y una clase"
+    return div;
+}
+
+document.body.appendChild(createDiv("container", "mainContainer"));
+
+function createSection(id:string):HTMLElement {
+    const section: HTMLElement = document.createElement("section") 
+    section.id = id;
+    section.textContent = "Una seccion con un id about";
+    return section;
+}
+document.body.appendChild(createSection("about"));
+
+function createArticle(id:string):HTMLElement {
+    const article: HTMLElement = document.createElement("article");
+    article.id = id;
+    article.textContent = "Un articuloo con un id post1";
+    return article;
+}
+document.body.appendChild(createArticle("post1"));
+
+function createFooter(clase:string):HTMLElement {
+    const footer:HTMLElement = document.createElement("footer");
+    footer.classList.add(clase);
+    footer.textContent = "Un footer con la clase pageFooter";
+    return footer;
+}
+document.body.appendChild(createFooter("pageFooter"))
+
+function createNav(aria_label:string):HTMLElement {
+    const nav : HTMLElement = document.createElement("nav");
+    nav.ariaLabel = aria_label;
+    nav.textContent = "Un nav con aria-label main navigation";
+    return nav;
+}
+document.body.appendChild(createNav("main navigation"))
+
 
 /**********Guardando cambio en el archivo********************* */
 await Bun.write("index.html", dom.serialize())
