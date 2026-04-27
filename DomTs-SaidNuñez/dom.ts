28a5dc67a6f8e20c3ff6c1fe700f3832684401ea
Saidale <said14alejandro14@gmail.com>
2026-04-27 13:19:40 -0600

    prueba

--- DomTs-SaidNuñez/dom.ts
+++ DomTs-SaidNuñez/dom.ts
@@ -40,7 +40,7 @@ for (const tech of technologies) {
 //Agrando un boton en el body
 const buttonId: HTMLButtonElement = document.createElement("button")
 
-buttonId.textContent = "Haz clic"
+buttonId.textContent = "VALE CAGADAAAAAAAAAAAAAAA"
 buttonId.id = "mainButton"
 buttonId.classList.add("primary")
 buttonId.setAttribute("onclick", "alert('xdxxd')");
